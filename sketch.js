906de094059798a3f7bb91dac32591fbdebf6dc8
Ethan <ethandeguire@gmail.com>
2019-08-04 23:14:37 -0400

    test

--- sketch.js
+++ sketch.js
@@ -4,21 +4,21 @@ var solving = false;
 
 //var gameState = 	'000001000000000000000006000000400000000080000209000007000000000000003000000000000';//illustration of how it solves it
 //var gameState = 	'000084010080050900140000285400003060006100009309800004000000401003400700010062300';//unsolvable, not enough givens
-var gameState = 	'009730526005020800608000047000009062040603080890500000260000108007010600951064200';
+var gameState   = 	'006008500000070613000000009000090001001000800400530000107053000050064000300100060';
 //var gameState = 	'020456789457080236689237040005362974274090653396574800040618397761040528938725060';
 
 function setup() {
 	frameRate(20);
 	createCanvas(windowWidth,windowHeight);
 	background(255);
-	
+
 	for (let i = 0; i<9; i++){
 		board[i] = [];
 		for (let j = 0; j<9; j++){
 			board[i][j] = new Board(j,i,gameState[(i*9)+j]);
 		}
 	}
-	
+
 	easierButtons('Solve',0,0,function(){solving = true;});
 	easierButtons('New Data',50,0,function(){gameState = ''+prompt();setup();});
 	easierButtons('Reset',125,0,function(){setup();});
@@ -30,13 +30,13 @@ function draw(){
 	let width;
 	if (colWidth > rowHeight) width = rowHeight;
 	if (colWidth < rowHeight) width = colWidth;
-	
+
 	for(let i = 0; i<9; i++){
 		for (let j = 0; j<9; j++){
 			board[i][j].show(width,width);
 		}
 	}
-	
+
 	if(solving == true){
 		for (let k = 0; k<5; k++){
 			for (let i = 0; i<9; i++){
@@ -59,11 +59,11 @@ function draw(){
 	line(width*6,0,width*6,width*9);
 	line(0,width*3,width*9,width*3)
 	line(0,width*6,width*9,width*6)
-	
+
 	noFill();
 	stroke(0);
 	rect(0,0,width*9,width*9);
-	
+
 	fill(255);
 	noStroke();
 	rect(width*9,0,windowWidth,windowHeight);
@@ -77,7 +77,7 @@ function returnRow(j){
 			str += (''+board[j][k].val)
 		}
 	}
-	return str;	
+	return str;
 }
 function returnCol(j){
 	let str = '';
@@ -86,7 +86,7 @@ function returnCol(j){
 			str += (''+board[k][j].val)
 		}
 	}
-	return str;	
+	return str;
 }
 function returnSquare(i,j){
 	let str = '';
@@ -100,7 +100,7 @@ function returnSquare(i,j){
 			}
 		}
 	}
-	return str;	
+	return str;
 }
 
 class Board{
@@ -110,20 +110,20 @@ class Board{
 		this.val = val;
 		if (this.val == '0')this.val = ' ';
 	}
-		
+
 	show(x,y){
 		fill(255);
 		stroke(20);
 		strokeWeight(3);
 		rect(this.i*x, this.j*y, (this.i+1)*x, (this.j+1)*y);
-		
+
 		fill(40);
 		stroke(40);
 		strokeWeight(.3);
 		textSize(30);
 		textAlign(CENTER);
 		text(this.val, (this.i*x + (this.i+1)*x)/2, (this.j*y + (this.j+1)*y)/2);
-		
+
 		/*show coords
 		fill(0);
 		stroke(0);
@@ -131,7 +131,7 @@ class Board{
 		text(this.i + ' ' + this.j, (this.i*x + (this.i+1)*x)/2, (this.j*y + (this.j+1)*y)/2+20);
 		*/
 	}
-	
+
 	check(){
 		let unsortedCantBeUsed = (returnRow(this.j)+''+returnCol(this.i)+''+returnSquare(this.j,this.i));
 		let cantBeUsed = sortAndRemove(unsortedCantBeUsed.split('')).toString().replace(/,/g,'');
